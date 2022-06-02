3336a6961eee8c3d53e673777c109ae59ea00307
Isaac Howard Jr <Isaachoward1112@gmail.com>
2022-06-02 13:30:05 -0400

    Initial commit

---
+++ test/s3_rep.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3Rep from '../lib/s3_rep-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_rep-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3Rep.S3RepStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
